97939ecebc20d7063a47200f7aba751220700be4
Alex Washburn <github@recursion.ninja>
2018-03-18 15:01:28 -0400

    Copying Alphabet data-structure from PCG.

---
+++ src/Data/Alphabet.hs
@@ -0,0 +1,54 @@
+-----------------------------------------------------------------------------
+-- |
+-- Module      :  Data.Alphabet
+-- Copyright   :  (c) 2015-2015 Ward Wheeler
+-- License     :  BSD-style
+--
+-- Maintainer  :  wheeler@amnh.org
+-- Stability   :  provisional
+-- Portability :  portable
+--
+-- An 'Alphabet' represents an /ordered/ list of unique symbols with constant
+-- time random access. Symbols are any data type which are coercable from a
+-- 'String' through the 'IsString' type-class.
+--
+-- An 'Alphabet' is constructed in one of two ways:
+--
+--  1. Supplying a `Foldable` structure of symbols which are 'IsString'
+--     instances to the 'fromSymbols' function.
+--
+--  2. Supplying a `Foldable` structure of symbols and state name pairs,
+--     both of which are 'IsString' instances to the 'fromSymbolsWithStateNames'
+--     function.
+--
+-- Both 'Alphabet' construction methods are order preserving with respect to the
+-- input symbol order.
+--
+-- Every 'Alphabet' contains a "gap" symbol denoted by the expression:
+-- > fromString "-"
+-- The "gap" character is always the last element in the ordered
+-- list regardless of its presence or position in the construction structure.
+--
+-- An 'Alphabet' will never contain the "missing" symbol denoted by the
+-- expression:
+-- > fromString "?"
+-- This symbol will be removed from the 'Alphabet'
+-- if it is present in the construction structure.
+-----------------------------------------------------------------------------   
+
+module Data.Alphabet
+  ( Alphabet()
+  , AmbiguityGroup
+  -- * Construction
+  , fromSymbols
+  , fromSymbolsWithStateNames
+  -- * Querries
+  , alphabetStateNames
+  , alphabetSymbols
+  , gapSymbol
+  -- * Truncation
+  , truncateAtSymbol
+  , truncateAtMaxSymbol
+  ) where
+
+import Data.Alphabet.Internal
