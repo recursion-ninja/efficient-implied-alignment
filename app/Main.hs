bd1f81ae6fc103a4618511d002b63aa7197683ef
Alex Washburn <github@recursion.ninja>
2018-03-18 17:19:57 -0400

    Changing argument ordering.

--- app/Main.hs
+++ app/Main.hs
@@ -9,9 +9,9 @@ import Text.PrettyPrint.ANSI.Leijen (string)
 
 data UserInput =
      UserInput
-     { treeFile   :: String
-     , dataFile   :: String
-     ,  tcmFile   :: String
+     { dataFile   :: String
+     , treeFile   :: String
+     , tcmFile    :: String
      , outputFile :: String
      , verbose    :: Bool
      } deriving (Show)
@@ -25,10 +25,10 @@ parseUserInput = customExecParser preferences $ info userInput description
   where
     userInput =
         UserInput
-          <$> argSpec 't' "tree"   "Newick tree file"
           <*> argSpec 'd' "data"   "FASTC data file"
+          <$> argSpec 't' "tree"   "Newick tree file"
           <*> argSpec 'm' "tcm"    "Transition Cost Matrix file with symbol alphabet"
-          <*> argSpec 'o' "output"  "Ouput file"
+          <*> argSpec 'o' "output"  "Output file for alignments"
           <*> switch  (mconcat [short 'v', long "verbose", help "Display debugging informaion"])
 
     argSpec c s h = strOption $ mconcat [short c, long s, help h, metavar (toUpper <$> s <> "FILE")]
