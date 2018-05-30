43e9551be99ce456d6ad5c136e28985e84fc6f05
Alex Washburn <github@recursion.ninja>
2018-05-29 20:25:18 -0400

    Adding node cost debigging output.

--- app/Main.hs
+++ app/Main.hs
@@ -38,10 +38,10 @@ runInput = do
             tree     = inputTree     fileInput
 
             maxLabelLen        = succ . maximum $ foldMapWithKey (\k _ -> [length k]) tree
-            inputRenderer  x i = mconcat [ padR maxLabelLen (i<> ":"), " ", renderSingleton alphabet $ x ^. preliminaryString ]
+            inputRenderer  x i = unwords [ padR maxLabelLen (i<> ":"), renderSingleton alphabet $ x ^. preliminaryString ]
 --            prelimRenderer x _ = mconcat [ padR maxLabelLen     "?:" , " ", renderSingleton alphabet $ x ^. preliminaryString ]
-            leafRenderer   x i = mconcat [ padR maxLabelLen (i<> ":"), " ", renderSingleton alphabet $ x ^. alignedString     ]
-            nodeRenderer   x _ = mconcat [ padR maxLabelLen     "?:" , " ", renderSingleton alphabet $ x ^. alignedString     ]
+            leafRenderer   x i = unwords [ padR maxLabelLen (i<> ":"), padL 3 . show $ x ^. localCost, padL 3 . show $ x ^. subtreeCost, renderSingleton alphabet $ x ^. alignedString ]
+            nodeRenderer   x _ = unwords [ padR maxLabelLen     "?:" , padL 3 . show $ x ^. localCost, padL 3 . show $ x ^. subtreeCost, renderSingleton alphabet $ x ^. alignedString ]
 
             postorder'    = postorder stringAligner
             preorder'     = preorder preorderRootLogic preorderInternalLogic preorderLeafLogic
