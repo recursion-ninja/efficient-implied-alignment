653a235633c6dc47377d1d4d82d6be0e22407010
Alex Washburn <github@recursion.ninja>
2019-10-04 14:27:08 -0400

    Adding preorder analysis and the results section

--- replicate-results.sh
+++ replicate-results.sh
@@ -7,7 +7,7 @@
     --output fungi-11 \
     -n [25,50,100,200,400,800,1553] \
     -k [1%1,1%2,1%4,1%8,1%16,1%32]
-python3 replicate-results/csv/fungi-11.preorder.csv replicate-results/png/fungi-preorder.png
+python3 plot-figure.py replicate-results/csv/fungi-11.preorder.csv replicate-results/png/fungi-preorder.png
 
 ./bin/generate-timings \
     --data data-sets/metazoa.afasta \
@@ -16,7 +16,7 @@ python3 replicate-results/csv/fungi-11.preorder.csv replicate-results/png/fungi-
     --output metazoa-11 \
     -n [25,50,100,200,400,800,1766] \
     -k [1%1,1%2,1%4,1%8,1%16,1%32]
-python3 replicate-results/csv/metazoa-11.preorder.csv replicate-results/png/metazoa-preorder.png
+python3 plot-figure.py replicate-results/csv/metazoa-11.preorder.csv replicate-results/png/metazoa-preorder.png
 
 ./bin/generate-timings \
     --data data-sets/pathological.afasta \
@@ -25,7 +25,7 @@ python3 replicate-results/csv/metazoa-11.preorder.csv replicate-results/png/meta
     --output pathological-12 \
     -n [4,8,16,32,64] \
     -k [1%1,1%2,1%4,1%8,1%16]
-python3 replicate-results/csv/pathological-12.preorder.csv replicate-results/png/pathological-12-preorder.png
+python3 plot-figure.py replicate-results/csv/pathological-12.preorder.csv replicate-results/png/pathological-12-preorder.png
 
 ./bin/generate-timings \
     --no-generate \
@@ -35,4 +35,4 @@ python3 replicate-results/csv/pathological-12.preorder.csv replicate-results/png
     --output pathological-31 \
     -n [4,8,16,32,64] \
     -k [1%1,1%2,1%4,1%8,1%16]
-python3 replicate-results/csv/pathological-31.preorder.csv replicate-results/png/pathological-31-preorder.png
+python3 plot-figure.py replicate-results/csv/pathological-31.preorder.csv replicate-results/png/pathological-31-preorder.png
