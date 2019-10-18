4a550c6bb494660891bd5b3d8278da10e8132bf6
recursion.ninja <github@recursion.ninja>
2019-10-17 23:01:33 -0400

    Because I don't know how to work r...

--- runtime-linear-model.R
+++ runtime-linear-model.R
@@ -1,7 +1,7 @@
 ### relationship between string length, leaf-set size and runtime
 
 ### set up work directory and load library----
-args = commandArgs(trailingOnly=TRUE)
+args <- commandArgs(trailingOnly=TRUE)
 if (length(args)==0) {
   stop("You must provide the input path and output file", call.=FALSE)
 } else if (length(args)==1) {
@@ -19,10 +19,10 @@ suppressWarnings(library(stargazer))
 
 ### best case data----
 ## read data
-best <- read.csv(paste(args[0], "pathological-12.preorder.csv", sep="/"),
+best <- read.csv(paste0(args[1], "/pathological-12.preorder.csv"),
                  colClasses = c("NULL", "NULL", NA, NA, NA),
                  col.names = c("null1", "null2","n", "k", "z"),
-                 HEADER=FALSE)
+                 header = FALSE)
 head(best) #sanity check
 sapply(best, class) #check column class
 
@@ -49,42 +49,42 @@ mod_best <- lm(data=best, log2(z)~log2(n)+log2(k))
 summary(mod_best)
 
 ### worst case data----
-worst <- read.csv(paste(args[0], "pathological-31.preorder.csv", sep="/"),
+worst <- read.csv(paste0(args[1], "/pathological-31.preorder.csv"),
                   colClasses = c("NULL", "NULL", NA, NA, NA),
                   col.names = c("null1", "null2","n", "k", "z"),
-                  HEADER=FALSE)
+                  header = FALSE)
 sapply(worst, class)
 head(worst)
 mod_worst <- lm(data=worst, log2(z)~log2(n)+log2(k))
 summary(mod_worst)
 
 ### fungi data----
-fungi <- read.csv(paste(args[0], "fungi-11.preorder.csv", sep="/"),
+fungi <- read.csv(paste0(args[1], "/fungi-11.preorder.csv"),
                   colClasses = c("NULL", "NULL", NA, NA, NA),
                   col.names = c("null1", "null2","n", "k", "z"),
-                  HEADER=FALSE)
+                  header = FALSE)
 sapply(fungi, class)
 mod_fungi <- lm(data=fungi, log2(z)~log2(n)+log2(k))
 summary(mod_fungi)
 
-### metazoa data----
-metazoa <- read.csv(paste(args[0], "metazoa-11.preorder.csv", sep="/"),
+## metazoa data----
+metazoa <- read.csv(paste0(args[1], "/metazoa-11.preorder.csv"),
                 colClasses = c("NULL", "NULL", NA, NA, NA),
                 col.names = c("null1", "null2","n", "k", "z"),
-                HEADER=FALSE)
+                header = FALSE)
 sapply(metazoa, class)
 mod_metazoa <- lm(data=metazoa, log2(z)~log2(n)+log2(k))
 summary(mod_metazoa)
 
 ### format all models, extract coefficients and adj. r2----
 # specify the output file to be .tex
-stargazer(mod_best, mod_worst, mod_fungi, mod_metazoa,
+stargazer(mod_best, mod_worst, mod_fungi, #mod_metazoa,
           type="latex",
           title="Regression coefficients of leaf-set size and string length on runtime",
           column.labels = c("Best", "Worst", "Fungi", "Metazoa"),
-          covariate.labels = c("$\log_2$ (String count $\\;n$)",
-                               "$\log_2$ (String length $k$)"),
-          dep.var.labels = "$\log_2$ (Runtime)",
+          covariate.labels = c("$\\log_2$ (String count $\\;n$)",
+                               "$\\log_2$ (String length $k$)"),
+          dep.var.labels = "$\\log_2$ (Runtime)",
           #keep=c("n", "k"),
           omit = "Constant",
           se=NULL, report="vc",
@@ -92,5 +92,5 @@ stargazer(mod_best, mod_worst, mod_fungi, mod_metazoa,
           omit.table.layout = "n",
           align=TRUE, #Requires \usepackage{dcolumn} in LaTeX preamble
           #omit.stat = c("f", "ser", "rsq"),
-          out=arg[1]) #"runtime-regression.tex")
+          out=args[2]) #"runtime-regression.tex")
 
