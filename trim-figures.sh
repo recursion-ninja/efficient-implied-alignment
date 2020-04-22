a01493cf2427f5997e4d39040e74c8e16fa0f323
Alex Washburn <github@recursion.ninja>
2020-04-22 10:46:44 -0400

    Trimming figures and adding a trimming script.

---
+++ trim-figures.sh
@@ -0,0 +1,4 @@
+convert -trim replicate-results/png/fungi-preorder.png replicate-results/png/fungi-preorder.png
+convert -trim replicate-results/png/metazoa-preorder.png replicate-results/png/metazoa-preorder.png
+convert -trim replicate-results/png/pathological-12-preorder.png replicate-results/png/pathological-12-preorder.png
+convert -trim replicate-results/png/pathological-31-preorder.png replicate-results/png/pathological-31-preorder.png
